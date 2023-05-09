259a7410b5d2d20385337607c429671a5aed0767
Joao-Vitor-Fernandes <joviramos16@gmail.com>
2023-05-09 15:52:12 -0300

    feito a tela de lista de questionários

--- src/pages/telaListaQuestionario.js
+++ src/pages/telaListaQuestionario.js
@@ -1,8 +1,44 @@
+import React from "react";
 import Window from "../components/Window";
+import "./telaListaQuestionario.css";
+
+import talk from "../icones/talk.png";
+
+const BotaoNivel = ({ nivel }) => {
+    let tipoQuiz, visivel;
+    if (nivel > 1) {
+      tipoQuiz = "divNivelQuizBloqueado";
+      visivel = true;
+    } else {
+      tipoQuiz = "divNivelQuiz";
+      visivel = false;
+    }
+    return (
+      <div style={{ marginTop: 25, marginBottom: 25 }}>
+        <img 
+            src={talk} alt="recycle" 
+            style={{
+                visibility: visivel ? "hidden" : "visible",
+                maxWidth: 25,
+                marginBottom: -20,
+                marginLeft: -23,
+            }}
+        />
+        <img
+          style={{ visibility: visivel ? "visible" : "hidden" }}
+          id="imagemCadeadoQuiz"
+          src="https://static.thenounproject.com/png/228611-200.png"
+        ></img>
+        <div className={tipoQuiz}>
+          <botao className="botaoNivelQuiz">{nivel}</botao>
+        </div>
+      </div>
+    );
+  };
 
 const TelaListaQuestionario = () => {
     return (
-        <Window titulo={"Tela de Reciclagem"}
+        <Window titulo={"Lista de Questionários"}
             styleWindow={{
                 height: "100vh",
                 width: "100vw",
@@ -12,8 +48,14 @@ const TelaListaQuestionario = () => {
                 // gap: 60,
             }}
         >
-            <div className="Recycle-window-cont" id="corpoRecycle">
-                <p>LISTA DE QUESTIONÁRIOS</p>
+            <div className="windowsBox" id="corpoQuestionarios">
+                <BotaoNivel nivel="7" />
+                <BotaoNivel nivel="6" />
+                <BotaoNivel nivel="5" />
+                <BotaoNivel nivel="4" />
+                <BotaoNivel nivel="3" />
+                <BotaoNivel nivel="2" />
+                <BotaoNivel nivel="1" />
             </div>
         </Window>
     )
