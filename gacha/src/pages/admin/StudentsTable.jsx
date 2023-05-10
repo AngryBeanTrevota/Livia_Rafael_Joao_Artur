b62d94ff4aa8b3ee178eb51a306fa957565200c7
razimbrao <rafaelozimbrao@gmail.com>
2023-05-10 17:29:51 -0300

    mexendo na tabela de estudantes

--- gacha/src/pages/admin/StudentsTable.jsx
+++ gacha/src/pages/admin/StudentsTable.jsx
@@ -107,85 +107,86 @@ function StudentsTable() {
   return (
     <>
       <NavbarAdm />
-    <div className="flex items-center flex-col w-full">
-      <div className="mt-12">
-        <h2 className="text-2xl font-bold mb-4">Lista de Estudantes</h2>
-      </div>
-      <div className="flex flex-col items-center">
-      <button
-        className=" self-start bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-        onClick={() => setCreateModalOpen(true)}>
-        Adicionar Estudante
-      </button>
-        <table className="table-auto my-5">
-          <thead>
-            <tr>
-              <th className="px-4 py-2">ID</th>
-              <th className="px-4 py-2">Nome</th>
-              <th className="px-4 py-2">Matrícula</th>
-              <th className="px-4 py-2">Ações</th>
-            </tr>
-          </thead>
-          <tbody>
-            {students.map((student) => (
-              <tr key={student.id}>
-                <td className="border px-4 py-2">{student.id}</td>
-                <td className="border px-4 py-2">{student.name}</td>
-                <td className="border px-4 py-2">{student.registerStudent}</td>
-                <td className="border px-4 py-2">
-                  <button
-                    className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-2 rounded"
-                    onClick={() => handleViewOpen(student)}>
-                    Visualizar
-                  </button>
-                  <button
-                    className="bg-yellow-500 hover:bg-yellow-700 text-white font-bold py-1 px-2 rounded mx-2"
-                    onClick={() => handleEditOpen(student)}>
-                    Editar
-                  </button>
-                  <button
-                    className="bg-red-500 hover:bg-red-700 text-white font-bold py-1 px-2 rounded"
-                    onClick={() => handleDelete(student.id)}>
-                    Excluir
-                  </button>
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      </div>
-      {selectedStudent && (
-        <div>
-          <ViewModal
-            isOpen={viewModalOpen}
-            onClose={() => setViewModalOpen(false)}
-            student={selectedStudent}
-          />
-          <EditModal
-            isOpen={editModalOpen}
-            onClose={() => setEditModalOpen(false)}
-            onSubmit={handleEditSubmit}
-            student={selectedStudent}
-            name={name}
-            onNameChange={handleNameChange}
-            register={register}
-            onRegisterChange={handleRegisterChange}
-            password={password}
-            onPasswordChange={handlePasswordChange}
-          />
+      <div className="flex items-center flex-col w-full">
+        <div className="mt-12">
+          <h2 className="text-2xl font-bold mb-4">Lista de Estudantes</h2>
         </div>
-      )}
-      {selectedStudent && (
-        <DeleteModal
-          isOpen={deleteModalOpen}
-          onClose={() => setDeleteModalOpen(false)}
-          onDelete={handleDelete}
-          student={selectedStudent}
-        />
-      )}
-    </div>
+        <div className="flex flex-col items-center">
+          <button
+            className=" self-start bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+            onClick={() => setCreateModalOpen(true)}>
+            Adicionar Estudante
+          </button>
+          <table className="table-auto my-5">
+            <thead>
+              <tr>
+                <th className="px-4 py-2">ID</th>
+                <th className="px-4 py-2">Nome</th>
+                <th className="px-4 py-2">Matrícula</th>
+                <th className="px-4 py-2">Ações</th>
+              </tr>
+            </thead>
+            <tbody>
+              {students.map((student) => (
+                <tr key={student.id}>
+                  <td className="border px-4 py-2">{student.id}</td>
+                  <td className="border px-4 py-2">{student.name}</td>
+                  <td className="border px-4 py-2">
+                    {student.registerStudent}
+                  </td>
+                  <td className="border px-4 py-2">
+                    <button
+                      className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-2 rounded"
+                      onClick={() => handleViewOpen(student)}>
+                      Visualizar
+                    </button>
+                    <button
+                      className="bg-yellow-500 hover:bg-yellow-700 text-white font-bold py-1 px-2 rounded mx-2"
+                      onClick={() => handleEditOpen(student)}>
+                      Editar
+                    </button>
+                    <button
+                      className="bg-red-500 hover:bg-red-700 text-white font-bold py-1 px-2 rounded"
+                      onClick={() => handleDelete(student.id)}>
+                      Excluir
+                    </button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+        {selectedStudent && (
+          <div>
+            <ViewModal
+              isOpen={viewModalOpen}
+              onClose={() => setViewModalOpen(false)}
+              student={selectedStudent}
+            />
+            <EditModal
+              isOpen={editModalOpen}
+              onClose={() => setEditModalOpen(false)}
+              onSubmit={handleEditSubmit}
+              student={selectedStudent}
+              name={name}
+              onNameChange={handleNameChange}
+              register={register}
+              onRegisterChange={handleRegisterChange}
+              password={password}
+              onPasswordChange={handlePasswordChange}
+            />
+          </div>
+        )}
+        {selectedStudent && (
+          <DeleteModal
+            isOpen={deleteModalOpen}
+            onClose={() => setDeleteModalOpen(false)}
+            onDelete={handleDelete}
+            student={selectedStudent}
+          />
+        )}
+      </div>
     </>
-
   );
 }
 
