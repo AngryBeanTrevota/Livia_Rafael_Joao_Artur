72351dfb56d6fa386a4517490d7fbc310f5fd316
Joao-Vitor-Fernandes <joviramos16@gmail.com>
2023-06-13 17:15:14 -0300

    fazendo ajustes e colocando o funcionamento do nome na janela de troca

--- gacha/src/pages/telaReciclagem.js
+++ gacha/src/pages/telaReciclagem.js
@@ -6,6 +6,9 @@ import recycle from "../icones/recycle.png";
 
 const TelaReciclagem = () => {
     const [popUpVisivel, setPopUpVisivel] = useState(false);
+    const [valorReciclagem, setValorReciclagem] = useState(0);
+    const [nomeVisivel, setNomeVisivel] = useState(false);
+    const [nomeItem, setNomeItem] = useState("");
 
     const abrePopUp = () => {
         setPopUpVisivel(true);
@@ -15,21 +18,28 @@ const TelaReciclagem = () => {
         setPopUpVisivel(false);
     };
 
-    const [valorReciclagem, setValorReciclagem] = useState(0);
-
     const itens = [
-        { nome: "Item 1", valorReciclagem: 5 },
-        { nome: "Item 2", valorReciclagem: 10 },
-        { nome: "Item 3", valorReciclagem: 8 },
-        { nome: "Item 4", valorReciclagem: 3 },
-        { nome: "Item 5", valorReciclagem: 7 },
-        { nome: "Item 6", valorReciclagem: 12 },
-        { nome: "Item 7", valorReciclagem: 23 },
-        { nome: "Item 8", valorReciclagem: 45 },
+        { nome: "Item 1", valorReciclagem: 5, quantidade: 2 },
+        { nome: "Item 2", valorReciclagem: 10, quantidade: 3 },
+        { nome: "Item 3", valorReciclagem: 8, quantidade: 2 },
+        { nome: "Item 4", valorReciclagem: 3, quantidade: 1 },
+        { nome: "Item 5", valorReciclagem: 7, quantidade: 2 },
+        { nome: "Item 6", valorReciclagem: 12, quantidade: 2 },
+        { nome: "Item 7", valorReciclagem: 23, quantidade: 4 },
+        { nome: "Item 8", valorReciclagem: 45, quantidade: 1 },
     ];
 
     const atualizarValorReciclagem = (valor) => {
         setValorReciclagem(valor);
+    };
+
+    const mostrarNomeItem = (nome) => {
+        setNomeItem(nome);
+        setNomeVisivel(true);
+    };
+    
+    const esconderNomeItem = () => {
+        setNomeVisivel(false);
     };
 
     return (
@@ -47,11 +57,11 @@ const TelaReciclagem = () => {
                 <div className="reciclar-painel">
                     <Window titulo={"RECICLAR"}
                         styleContainer={{
-                            width: 500,
+                            width: 300,
                             height: 400,
                             position: "absolute",
                             top: "20%",
-                            left: "28%",
+                            left: "35%",
                         }}
                     >
                         <div className="recycle-cont">
@@ -60,6 +70,7 @@ const TelaReciclagem = () => {
                             <div
                                 className="itens-folder"
                                 onMouseEnter={() => atualizarValorReciclagem(item.valorReciclagem)}
+                                onClick={() => mostrarNomeItem(item.nome)}
                                 key={index}
                             >
                                 <img
@@ -68,7 +79,7 @@ const TelaReciclagem = () => {
                                     src="https://i.imgur.com/r3a0P0E.png"
                                 />
                                 <p className="item-name">{item.nome}</p>
-                                <p>3x</p>
+                                <p>{item.quantidade}x</p>
                             </div>
                             ))}
                              </div>
@@ -91,6 +102,11 @@ const TelaReciclagem = () => {
                             <p className="tiros-text">{valorReciclagem} Tiros</p>
                         </div>
                         <div className="button-troca-cont">
+                            {nomeVisivel && (
+                                <div className="item-nome-visivel">
+                                <p>{nomeItem}</p>
+                                </div>
+                            )}
                             <button className="button-troca"
                                 onClick={abrePopUp}
                             >
@@ -121,6 +137,7 @@ const TelaReciclagem = () => {
                         <div className="button-troca-cont">
                             <button className="button-troca"
                                 onClick={fechaPopUp}
+                                onMouseLeave={esconderNomeItem}
                             >
                                 <p>OK</p>
                             </button>
